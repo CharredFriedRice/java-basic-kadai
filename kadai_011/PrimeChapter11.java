2c0c7f554fc689f7b1dc7003cbd544861b0f82d2
中村優太 <owtis666@gmail.com>
2023-09-15 20:56:33 +0900

    kadai_011を修正

--- kadai_011/PrimeChapter11.java
+++ kadai_011/PrimeChapter11.java
@@ -1,34 +1,34 @@
 package kadai_011;
 
 public class PrimeChapter11 {
+	
+	public static boolean prime(int num) 
+	{
+		for(int i = 2; i < num; i++)
+		{
+			int divide = num % i;
+			if(divide == 0 && i < num) 
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 
 	public static void main(String[] args) 
 	{
 		boolean primeNumber[] = new boolean[101];
-		int falseCnt = 0;
-		int num = 7;
-		for(int divide = 2; divide < primeNumber.length ; divide++)
+		
+		for(int i = 2; i <= 100; i++) 
 		{
-			if(num % divide == 0)
+			primeNumber[i] = prime(i);
+		}
+		for(int a = 0; a < primeNumber.length; a++)
+		{
+			if(primeNumber[a] == true) 
 			{
-				primeNumber[divide] = false;
-				falseCnt++;
-			}
-			else 
-			{
-				primeNumber[divide] = true;
-			}
-			
-			System.out.println(divide + ":" + primeNumber[divide]);
-			
-			if(divide == primeNumber.length - 1) 
-			{
-				if(primeNumber[num] == false && falseCnt == 1) 
-				{
-					System.out.println(num + "は素数です。");
-				}
+				System.out.println(a);
 			}
 		}
 	}
-
 }
