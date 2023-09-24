7ccae543b3e3b8a77fcaa944e09c27c86913ac24
中村優太 <owtis666@gmail.com>
2023-09-24 11:00:11 +0900

    kadai_020を追加

---
+++ kadai_020/DictionaryExec_Chapter21.java
@@ -0,0 +1,27 @@
+package kadai_020;
+
+import java.util.HashMap;
+
+public class DictionaryExec_Chapter21 {
+
+	public void test(String word) {
+		HashMap<String, String> hashFruits = new HashMap<String, String>();
+		hashFruits.put("apple", "りんご");
+		hashFruits.put("peach", "桃");
+		hashFruits.put("banana", "バナナ");
+		hashFruits.put("lemon", "レモン");
+		hashFruits.put("pear", "梨");
+		hashFruits.put("kiwi", "キウィ");
+		hashFruits.put("strawberry", "いちご");
+		hashFruits.put("grape", "ぶどう");
+		hashFruits.put("muscat", "マスカット");
+		hashFruits.put("cherry", "さくらんぼ");
+		
+		if(hashFruits.get(word) != null) {
+			System.out.println(hashFruits.get(word));
+		}
+		else {
+			System.out.println("辞書に" + word + "は存在しません。");
+		}
+	}
+}
---
+++ kadai_020/Dictionary_Chapter20.java
@@ -0,0 +1,12 @@
+package kadai_020;
+
+public class Dictionary_Chapter20 {
+
+	public static void main(String[] args) {
+		DictionaryExec_Chapter21 chapter21 = new DictionaryExec_Chapter21();
+		String[] fruits = {"apple","banana","grape","orange"};
+		for(String word : fruits) {
+			chapter21.test(word);
+		}
+	}
+}
